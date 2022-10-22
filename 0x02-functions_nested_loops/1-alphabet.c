a24971063b01e203c45206c09c6f6c2283fee8ba
Oluwasegun Michael <michaelaykay@gmail.com>
2022-10-22 15:50:23 -0700

    Fraud

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(0)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
