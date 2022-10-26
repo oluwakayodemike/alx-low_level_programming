24587d124e27505550c51e3135105c2f04421ff9
Oluwasegun Michael <michaelaykay@gmail.com>
2022-10-26 02:23:42 -0700

    Goat

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - entry point
- *
- * Description: Prints the alphabet with _putchar ten times
+ * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: void
  */
 
-
 void print_alphabet_x10(void)
 {
-	char c;
-	int i;
+	int round = 0;
+	char letter = 'a';
 
-	for (i = 0; i < 10; i++)
+	while (round < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		letter = 'a';
+		while (letter <= 'z')
 		{
-			_putchar(c);
+			_putchar(letter);
+			letter++;
 		}
 		_putchar('\n');
+
+		round++;
 	}
 }
