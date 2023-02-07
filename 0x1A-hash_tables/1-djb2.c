f1bf12e55de39c2789ffeabf0fde2eebaf44b971
Oluwakayode Michael <michaelaykay@gmail.com>
2023-02-07 03:15:35 -0800

    1. djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,13 +10,11 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-
 	int count;
 
 	hash = 5381;
 	while ((count = *str++))
-		hash = ((hash << 5) + hash) + count; 
-	/* hash * 33 + count */
+		hash = ((hash << 5) + hash) + count;/* hash * 33 + count */
 
 	return (hash);
 }
