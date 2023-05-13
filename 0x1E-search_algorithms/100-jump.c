4e02b3fabc3e0d67292a416e5b133fe279288f82
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-13 15:21:55 -0700

    7. Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,46 +1,50 @@
-#include <stdio.h>
+#include <math.h>
 #include "search_algos.h"
 
 /**
- * jump_search - Searches for a value in a sorted array of integers
- *               using the Jump search algorithm.
- * @array: Pointer to the first element of the array to search in.
- * @size: Number of elements in the array.
- * @value: The value to search for.
+ * jump_search - search a sorted array for a value using jump search method
+ * @array: the array to be searched
+ * @size: the size of the array (number of elements)
+ * @value: the int to be searched for
  *
- * Return: The index where the value is located, or -1 if not found
+ * Return: 1st index value is found at or -1 if not present or null array
  */
 int jump_search(int *array, size_t size, int value)
 {
 	int jump = sqrt(size);
 	int start = 0;
-	int end = jump - 1;
+	int section_end = 0;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size == 0 || array[start] > value)
 		return (-1);
 
-	while (end < (int)size)
+	while (section_end < (int)size)
 	{
-		if (array[end] >= value)
-			break;
+		if (array[section_end] < value)
+		{
+			printf("Value checked array[%d] = [%d]\n", section_end, array[section_end]);
 
-		printf("Value checked array[%d] = [%d]\n", end, array[end]);
-		start = end;
-		end += jump;
+			start = section_end;
+			section_end += jump;
+		}
+		else
+		{
+			break;
+		}
 	}
 
-	printf("Value found between indexes [%d] and [%d]\n", start, end);
+	printf("Value found between indexes [%d] and [%d]\n", start, section_end);
 
-	if (end > (int)size - 1)
-		end = size - 1;
+	if (section_end > (int)size - 1)
+		section_end = size - 1;
 
-	for (; start <= end; start++)
+	while (start <= section_end)
 	{
 		printf("Value checked array[%d] = [%d]\n", start, array[start]);
 		if (array[start] == value)
 			return (start);
+		start++;
 	}
 
 	return (-1);
 }
-
